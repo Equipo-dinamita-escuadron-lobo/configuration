e1ee298bb3f218bc7d2eaf78f57171c6ffc178ae
santiagonieto09 <santiago.nieto09@gmail.com>
2025-08-08 15:25:22 -0500

    feat: Base microservicio configuracion

---
+++ src/test/java/co/unicauca/edu/co/contables/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.co.contables.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

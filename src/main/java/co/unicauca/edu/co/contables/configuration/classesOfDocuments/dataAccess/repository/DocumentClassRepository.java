34bc88e88a367575784807725d71a960fcfa14d5
santiagonieto09 <santiago.nieto09@gmail.com>
2025-08-13 09:37:48 -0500

    remove: findAllByIdEnterprise sin paginacion

--- src/main/java/co/unicauca/edu/co/contables/configuration/classesOfDocuments/dataAccess/repository/DocumentClassRepository.java
+++ src/main/java/co/unicauca/edu/co/contables/configuration/classesOfDocuments/dataAccess/repository/DocumentClassRepository.java
@@ -4,14 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 import co.unicauca.edu.co.contables.configuration.classesOfDocuments.dataAccess.entity.DocumentClassEntity;
 
 public interface DocumentClassRepository extends JpaRepository<DocumentClassEntity, Long> {
     boolean existsByNameAndIdEnterprise(String name, String idEnterprise);
     boolean existsByNameAndIdEnterpriseAndIdNot(String name, String idEnterprise, Long id);
-    List<DocumentClassEntity> findAllByIdEnterprise(String idEnterprise);
     Page<DocumentClassEntity> findAllByIdEnterprise(String idEnterprise, Pageable pageable);
 }
 
